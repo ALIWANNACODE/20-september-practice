07e0f16c69f1c5e7bd71fc8ca9480cb5f3afbe6e
aliwannacode <malitariq360@gmail.com>
2024-09-20 19:56:25 +0500

    project complete

---
+++ app.js
@@ -0,0 +1,78 @@
+console.log("Hello world!");
+
+console.log(10 / 5);
+
+console.log("hello Javascript")
+
+
+console.log(2 + 2);
+console.log("2" + "2");
+
+
+
+console.log(2 + 2);
+
+console.log("2a" + "2b");
+
+console.log("My name is Muhammad Ali")
+
+console.log(true)
+console.log(20)
+console.log("20")
+
+
+
+alert("Hello world!");
+
+alert(2 + 2);
+
+
+console.log('abc');
+
+
+var city = "karachi";
+
+var username = 'ali';
+var age = 20;
+var isLoggedIn = true;
+
+console.log(username , age , isLoggedIn);
+
+
+
+var num1 = "20";
+var num2 = "30";
+console.log(num1 + num2);
+
+
+
+var fullName = 'ali';
+
+console.log(fullName);
+
+
+var username;
+console.log(username);
+
+
+var city = 'karachi'; 
+console.log(city);
+
+var city = 'islamabad'; 
+console.log(city);
+
+city = 'lahore'; 
+console.log(city);
+
+
+
+var weight = 29;
+console.log(weight + 30)
+
+
+
+var fullname = 'ali'
+
+
+var rose = 'tulip';
+var Rose = 'abc'
